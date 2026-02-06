392a16069f798cb55f5e949f0e044347f5acdfdd
attila45 <92511408+attila45@users.noreply.github.com>
2026-02-06 08:15:12 +0100

    finished console demo for user DAO

--- RecipeSharingJava/src/main/java/com/pogany/recipesharingforum/Main.java
+++ RecipeSharingJava/src/main/java/com/pogany/recipesharingforum/Main.java
@@ -62,7 +62,70 @@ public class Main {
     }
 
     //above is the selector, below are the soon to be console demos
-    private static void userMenu(Scanner sc, UserDao userDao) throws SQLException {}
+    private static void userMenu(Scanner sc, UserDao userDao) throws SQLException {
+        System.out.println("""
+                --- USER MENU ---
+                1 - Create user
+                2 - Update user
+                3 - Remove user
+                4 - Find all
+                5 - Find by ID
+                6 - Find by login
+                7 - Find by email
+                """);
+
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        switch (choice) {
+            case 1 -> {
+                System.out.print("Login: ");
+                String login = sc.nextLine();
+                System.out.print("Password: ");
+                String pass = sc.nextLine();
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("Country: ");
+                String country = sc.nextLine();
+
+                userDao.createUser(new User(login, pass, email, country));
+            }
+            case 2 -> {
+                System.out.print("Login: ");
+                String login = sc.nextLine();
+                System.out.print("Password: ");
+                String pass = sc.nextLine();
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("Country: ");
+                String country = sc.nextLine();
+
+                userDao.updateUser(new User(login, pass, email, country));
+            }
+            case 3 -> {
+                System.out.print("ID: ");
+                int id = sc.nextInt();
+                userDao.removeUser(new User(id, null, null, null, null));
+            }
+            case 4 -> {
+                List<User> users = userDao.findAll();
+                users.forEach(u ->
+                        System.out.println(u.getId() + " " + u.getLogin() + " " + u.getEmail()));
+            }
+            case 5 -> {
+                System.out.print("ID: ");
+                System.out.println(userDao.findById(sc.nextInt()));
+            }
+            case 6 -> {
+                System.out.print("Login: ");
+                System.out.println(userDao.findByLogin(sc.nextLine()));
+            }
+            case 7 -> {
+                System.out.print("Email: ");
+                System.out.println(userDao.findByEmail(sc.nextLine()));
+            }
+        }
+    }
     private static void postMenu(Scanner sc, PostDao postDao) throws SQLException {}
     private static void commentMenu(Scanner sc, CommentDao commentDao) throws SQLException {}
 
