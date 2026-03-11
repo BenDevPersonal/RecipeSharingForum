d83b4eb91c4fecbf19d6047f2b2268515076af2b
Takacs Bence <bence.takacs2006@gmail.com>
2026-03-11 12:21:14 +0100

    Replaced project with spring boot generated project

---
+++ RecipeSharingJava/src/main/java/com/pogany/RecipeSharingJava/RecipeSharingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pogany.RecipeSharingJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSharingJavaApplication.class, args);
+	}
+
+}
