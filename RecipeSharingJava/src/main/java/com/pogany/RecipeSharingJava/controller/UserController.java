0ff8a19bc41a5dc58ac90c2e67c3ab6f9cc2bcfb
Takacs Bence <bence.takacs2006@gmail.com>
2026-03-19 12:15:18 +0100

    Made UserController and CreateUserRequest DTO

--- RecipeSharingJava/src/main/java/com/pogany/RecipeSharingJava/controller/UserController.java
+++ RecipeSharingJava/src/main/java/com/pogany/RecipeSharingJava/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.pogany.RecipeSharingJava.controller;
 
+import com.pogany.RecipeSharingJava.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/${id}")
+    public UserDto getUserById(@PathVariable Integer id) {
+        return userService.findById(id);
+    }
 }
---
+++ RecipeSharingJava/src/main/java/com/pogany/RecipeSharingJava/dto/CreateUserRequest.java
@@ -0,0 +1,54 @@
+package com.pogany.RecipeSharingJava.dto;
+
+import com.pogany.RecipeSharingJava.entity.Role;
+
+public class CreateUserRequest {
+    private String login;
+    private String email;
+    private String password;
+    private String country;
+    private Role role;
+
+    public CreateUserRequest() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
